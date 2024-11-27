2eac887729c71cb3ca2ca8b223bd45f76f0e30e1
nin2323 <juansobraoperez@gmail.com>
2024-11-27 14:28:41 +0100

    exercises arrays

---
+++ exercisesarrays.js
@@ -0,0 +1,7 @@
+//  Ejercicios Arrayas
+/*
+  1. Crea un array que contenga un listado de tareas. Las tareas tienen un texto y pueden estar completadas o pendientes. 
+*/
+const tareas = ['limpiar', 'recoger', 'comprar'];
+console.log(typeof tareas);
+console.log(Array .isArray(tareas))     
