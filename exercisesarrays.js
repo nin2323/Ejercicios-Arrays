a9d33bb037fba20201cc4b70c7d90f1a377d4aab
nin2323 <juansobraoperez@gmail.com>
2024-11-28 15:42:14 +0100

    exercises arrays

--- exercisesarrays.js
+++ exercisesarrays.js
@@ -2,6 +2,57 @@
 /*
   1. Crea un array que contenga un listado de tareas. Las tareas tienen un texto y pueden estar completadas o pendientes. 
 */
-const tareas = ['limpiar', 'recoger', 'comprar'];
-console.log(typeof tareas);
-console.log(Array .isArray(tareas))    
+const tareas = ['tarea1', 'tarea2', 'tarea3'];
+const tarea1 = {
+    tittle: 'recoger',
+    condition: 'complete',
+} 
+const tarea2 = {
+  tittle: 'limpiar',
+  condition: 'pending',
+} 
+const tarea3 = {
+  tittle: 'comprar',
+  condition: 'complete',
+} 
+
+/*
+  2. Crea un array que contenga un listado de vehículos. Usa al menos 5 propiedades en cada vehículo. 
+*/
+const vehiculos = ['mercedes', 'ford', 'fiat'];
+const mercedes = {
+  color: 'blue',
+  size: 'big',
+  year: '2015',
+  motor: 'gasoil',
+  type: 'manual',
+}
+const ford = {
+  color: 'black',
+  size: 'small',
+  year: '2021',
+  motor: 'electric',
+  type: 'automatic',
+}
+const fiat = {
+  color: 'red',
+  size: 'normal',
+  year: '2018',
+  motor: 'gasoil',
+  type: 'manual'
+}
+console.log(vehiculos);
+
+/*
+  3. Imagina que estás en un supermercado haciendo la compra y tienes que comprar lo que está 
+  en la variable shoppingList.
+
+  Crea una función a la que le pases dos parámetros, el array de la compra, y el elemento que has comprado
+  esa función debe buscar el elemento en el array y eliminarlo.
+
+  Por ejemplo, si le paso un array ['pepino', 'tomate', 'sandía'] y el string 'tomate', debe devolver un array ['pepino', 'sandía']
+*/
+ 
+  
+
+
